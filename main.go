8f3acde65830296ff75512e01ca20c049e73328e
Peter Hellberg <peter@c7.se>
2025-12-20 17:36:04 +0100

    cmd: Handle fingerprint in build.zig.zon

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"embed"
 	"flag"
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -17,6 +20,12 @@ type config struct {
 	dir      string
 	bin      string
 	hostname string
+	zon      ZON
+}
+
+type ZON struct {
+	name        string
+	fingerprint string
 }
 
 func main() {
@@ -26,7 +35,7 @@ func main() {
 	}
 }
 
-func run(args []string, stdout io.Writer) error {
+func parse(args []string, stderr io.Writer) (config, error) {
 	var cfg config
 
 	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
@@ -43,17 +52,33 @@ func run(args []string, stdout io.Writer) error {
 	flags.StringVar(&cfg.hostname, "hostname", "peter.tilde.team", "Deploy to hostname using SSH/SCP")
 
 	if err := flags.Parse(args[1:]); err != nil {
-		return err
+		return cfg, err
 	}
 
 	rest := flags.Args()
 
 	// Require a directory name
 	if len(rest) < 1 {
-		return fmt.Errorf("no name given as the first argument")
+		return cfg, fmt.Errorf("no name given as the first argument")
 	}
 
 	cfg.dir = rest[0]
+
+	zon, err := initZON(cfg.dir)
+	if err != nil {
+		return cfg, err
+	}
+
+	cfg.zon = zon
+
+	return cfg, nil
+}
+
+func run(args []string, stderr io.Writer) error {
+	cfg, err := parse(args, stderr)
+	if err != nil {
+		return err
+	}
 
 	// Make sure that dir does not already exist
 	if _, err := os.Stat(cfg.dir); !os.IsNotExist(err) {
@@ -121,8 +146,11 @@ func replacer(cfg config, name string, data []byte) []byte {
 	switch name {
 	case "build.zig":
 		return replaceOne(data, "tic80-pro", cfg.bin)
-	case "build.zig.zon", "README.md":
+	case "README.md":
 		return replaceOne(data, "tic80-zig-cart", cfg.dir)
+	case "build.zig.zon":
+		data = replaceOne(data, ".tic80_zig_cart", cfg.zon.name)
+		return replaceOne(data, "0x588392c398b4bdc", cfg.zon.fingerprint)
 	case "cart.wasmp":
 		return replaceOne(data, "TIC-80 Zig Cart", cfg.dir)
 	case "Makefile":
@@ -135,4 +163,65 @@ func replacer(cfg config, name string, data []byte) []byte {
 
 func replaceOne(data []byte, old, new string) []byte {
 	return bytes.Replace(data, []byte(old), []byte(new), 1)
+}
+
+func initZON(dir string) (ZON, error) {
+	tmp, err := os.MkdirTemp("", "w4-init-")
+	if err != nil {
+		return ZON{}, err
+	}
+	defer os.RemoveAll(tmp)
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		return ZON{}, err
+	}
+	defer os.Chdir(cwd)
+
+	tmpDir := filepath.Join(tmp, dir)
+
+	if err := os.Mkdir(tmpDir, 0o755); err != nil {
+		return ZON{}, err
+	}
+
+	if err := os.Chdir(tmpDir); err != nil {
+		return ZON{}, err
+	}
+
+	cmd := exec.Command("zig", "init")
+
+	if err := cmd.Run(); err != nil {
+		return ZON{}, err
+	}
+
+	zonPath := filepath.Join(tmpDir, "build.zig.zon")
+
+	return extractZON(zonPath)
+}
+
+func extractZON(zonPath string) (ZON, error) {
+	var zon ZON
+
+	f, err := os.Open(zonPath)
+	if err != nil {
+		return zon, err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		text := strings.TrimSpace(scanner.Text())
+
+		if prefix := ".name = "; strings.Contains(text, prefix) {
+			zon.name = strings.TrimSuffix(strings.TrimPrefix(text, prefix), ",")
+		}
+
+		if prefix := ".fingerprint = "; strings.Contains(text, prefix) {
+			fingerprint, _, _ := strings.Cut(strings.TrimPrefix(text, prefix), ",")
+			zon.fingerprint = fingerprint
+		}
+	}
+
+	return zon, nil
 }
